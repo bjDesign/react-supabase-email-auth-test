a0352f97bed8d887c87cc6e73cfa3a7f26c4919b
Bill Jordan <bill@billjordandesign.com>
2025-02-04 17:09:57 -0500

    moving right along

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { UserAuth } from '../context/AuthContext';
 
 
@@ -10,10 +10,29 @@ export const SignUp = () => {
     const [loading, setLoading] = useState("");
 
     const { session, signUpNewUser } = UserAuth();
+    const navigate = useNavigate();
+
     console.log(session);
+
+    const handleSignUp = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        try {
+            const result = await signUpNewUser(email, password);
+
+            if(result.success) {
+                navigate('/dashboard')
+            }
+        } catch (err) {
+            setError("an error occurred");
+        } finally {
+            setLoading(false);
+        }
+    }
+
   return (
     <div>
-        <form className='max-w-md m-auto pt-24'>
+        <form onSubmit={handleSignUp} className='max-w-md m-auto pt-24'>
             <h2 className='font-bold pb-2'>
                 Sign Up Today!
             </h2>
@@ -21,9 +40,10 @@ export const SignUp = () => {
                 Already have an account? <Link to="/signin">Sign In here</Link> 
             </p>
             <div className='flex flex-col py-4'>
-                <input placeholder='Email' className='p-3 mt-2 bg-black' type="email" />
-                <input placeholder='Password' className='p-3 mt-2 bg-black' type="password" />
+                <input onChange={(e) => setEmail(e.target.value)} placeholder='Email' className='p-3 mt-2 bg-black' type="email" />
+                <input onChange={(e) => setPassword(e.target.value)} placeholder='Password' className='p-3 mt-2 bg-black' type="password" />
                 <button type='submit' disabled={loading} className='mt-4 w-full'>Sign Up</button>
+                { error & <p className='text-red-600 text-center pt-4'>{error}</p>}
             </div>
         </form>
     </div>
